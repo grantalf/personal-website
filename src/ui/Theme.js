5057b197893616502941f01b43ea4177013ae7ba
Grant Harris <grantxvx@gmail.com>
2021-01-12 22:04:27 -0600

    put quotes around typography.tab.color

--- src/ui/Theme.js
+++ src/ui/Theme.js
@@ -23,6 +23,7 @@ export default createMuiTheme({
       textTransform: 'none',
       fontWeight: 700,
       fontSize: '1rem',
+      color: '#fff',
     },
     hire: {
       fontFamily: 'Pacifico',
